07d35626b7fbc3e5a9fb98935575ab0bc08c4cb0
Thomas Houlier <thoulier@enseirb-matmeca.fr>
2020-11-13 11:17:12 +0100

    Debut lecture machine file

--- Phase1/dsmexec.c
+++ Phase1/dsmexec.c
@@ -25,89 +25,116 @@ void sigchld_handler(int sig)
 
 int main(int argc, char *argv[])
 {
-  if (argc < 3){
-    usage();
-  } else {       
-     pid_t pid;
-     int num_procs = 0;
-     int i;
-     
-     /* Mise en place d'un traitant pour recuperer les fils zombies*/      
-     /* XXX.sa_handler = sigchld_handler; */
-     
-     /* lecture du fichier de machines */
-     /* 1- on recupere le nombre de processus a lancer */
-     /* 2- on recupere les noms des machines : le nom de */
-     /* la machine est un des elements d'identification */
-     
-     /* creation de la socket d'ecoute */
-     /* + ecoute effective */ 
-     
-     /* creation des fils */
-     for(i = 0; i < num_procs ; i++) {
-	
-	/* creation du tube pour rediriger stdout */
-	
-	/* creation du tube pour rediriger stderr */
-	
-	pid = fork();
-	if(pid == -1) ERROR_EXIT("fork");
-	
-	if (pid == 0) { /* fils */	
-	   
-	   /* redirection stdout */	      
-	   
-	   /* redirection stderr */	      	      
-	   
-	   /* Creation du tableau d'arguments pour le ssh */ 
-	   
-	   /* jump to new prog : */
-	   /* execvp("ssh",newargv); */
+   if (argc < 3){
+      usage();
+   } else {       
+      pid_t pid;
+      int num_procs = 0;
+      int i;
+      
+      /* Mise en place d'un traitant pour recuperer les fils zombies*/      
+      /* XXX.sa_handler = sigchld_handler; */
+      /*int fd_out;
+      if (-1 == (fd_out = open("machine_file",O_RDONLY))){
+         perror("Error on opening machine file");
+      } */
 
-	} else  if(pid > 0) { /* pere */		      
-	   /* fermeture des extremites des tubes non utiles */
-	   num_procs_creat++;	      
-	}
-     }
-     
-   
-     for(i = 0; i < num_procs ; i++){
-	
-	/* on accepte les connexions des processus dsm */
-	
-	/*  On recupere le nom de la machine distante */
-	/* 1- d'abord la taille de la chaine */
-	/* 2- puis la chaine elle-meme */
-	
-	/* On recupere le pid du processus distant  */
-	
-	/* On recupere le numero de port de la socket */
-	/* d'ecoute des processus distants */
-     }
-     
-     /* envoi du nombre de processus aux processus dsm*/
-     
-     /* envoi des rangs aux processus dsm */
-     
-     /* envoi des infos de connexion aux processus */
-     
-     /* gestion des E/S : on recupere les caracteres */
-     /* sur les tubes de redirection de stdout/stderr */     
-     /* while(1)
-         {
-            je recupere les infos sur les tubes de redirection
-            jusqu'à ce qu'ils soient inactifs (ie fermes par les
-            processus dsm ecrivains de l'autre cote ...)
-       
-         };
-      */
-     
-     /* on attend les processus fils */
-     
-     /* on ferme les descripteurs proprement */
-     
-     /* on ferme la socket d'ecoute */
-  }   
+
+      /* lecture du fichier de machines */
+      FILE * fichier = NULL;
+      char buff[128];
+      memset(buff,0,128);
+      char tab_machine_name[3][128];
+
+      fichier = fopen("machine_file","r");
+      if (fichier != NULL){
+         while (fgets(buff, 128, fichier) != NULL){
+            num_procs_creat ++;
+            strcpy(tab_machine_name[num_procs_creat],buff,strlen(buff));
+            printf("%s\n", buff);
+         }
+      }
+      fclose(fichier);
+      printf("nb de processus à creer : %d\n", num_procs_creat);
+      for (int i=0; i<num_procs_creat; i++){
+         printf("machines : %s\n", tab_machine_name[num_procs_creat]);
+      }
+
+      /* 1- on recupere le nombre de processus a lancer */
+
+      /* 2- on recupere les noms des machines : le nom de */
+      
+
+      /* la machine est un des elements d'identification */
+      
+      /* creation de la socket d'ecoute */
+      /* + ecoute effective */ 
+      
+      /* creation des fils */
+      for(i = 0; i < num_procs ; i++) {
+      
+      /* creation du tube pour rediriger stdout */
+      
+      /* creation du tube pour rediriger stderr */
+      
+      pid = fork();
+      if(pid == -1) ERROR_EXIT("fork");
+      
+      if (pid == 0) { /* fils */	
+         
+         /* redirection stdout */	      
+         
+         /* redirection stderr */	      	      
+         
+         /* Creation du tableau d'arguments pour le ssh */ 
+         
+         /* jump to new prog : */
+         /* execvp("ssh",newargv); */
+
+      } else  if(pid > 0) { /* pere */		      
+         /* fermeture des extremites des tubes non utiles */
+         num_procs_creat++;	      
+      }
+      }
+      
+      
+      for(i = 0; i < num_procs ; i++){
+      
+      /* on accepte les connexions des processus dsm */
+      
+      /*  On recupere le nom de la machine distante */
+      /* 1- d'abord la taille de la chaine */
+      /* 2- puis la chaine elle-meme */
+      
+      /* On recupere le pid du processus distant  */
+      
+      /* On recupere le numero de port de la socket */
+      /* d'ecoute des processus distants */
+      }
+      
+      /* envoi du nombre de processus aux processus dsm*/
+      
+      /* envoi des rangs aux processus dsm */
+      
+      /* envoi des infos de connexion aux processus */
+      
+      /* gestion des E/S : on recupere les caracteres */
+      /* sur les tubes de redirection de stdout/stderr */     
+      /* while(1)
+            {
+               je recupere les infos sur les tubes de redirection
+               jusqu'à ce qu'ils soient inactifs (ie fermes par les
+               processus dsm ecrivains de l'autre cote ...)
+         
+            };
+         */
+      
+      /* on attend les processus fils */
+      
+      /* on ferme les descripteurs proprement */
+      
+      /* on ferme la socket d'ecoute */
+   }   
    exit(EXIT_SUCCESS);  
 }
 
