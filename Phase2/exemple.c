79aa2c857667a3571d271e8efe6d334d2ea21cb5
Thomas Houlier <thoulier@enseirb-matmeca.fr>
2021-01-08 16:47:58 +0100

    last commit

--- Phase2/exemple.c
+++ Phase2/exemple.c
@@ -2,33 +2,33 @@
 
 int main(int argc, char **argv)
 {
-   char *pointer;
-   char *current;
-   int value;
+    char *pointer;
+    char *current;
+    int value;
     printf("----------------------DEBUT EXEMPLE-------------------------\n");
     fflush(stdout);
     pointer = dsm_init(argc,argv);
-   current = pointer;
+    current = pointer;
 
-   printf("[%i] Coucou, mon adresse de base est : %p\n", DSM_NODE_ID, pointer);
+    printf("[%i] Coucou, mon adresse de base est : %p\n", DSM_NODE_ID, pointer);
 
-   if (DSM_NODE_ID == 0)
-     {
-       current += 4*sizeof(int);
-       value = *((int *)current);
-       printf("[%i] valeur de l'entier : %i\n", DSM_NODE_ID, value);
-     }
-   else if (DSM_NODE_ID == 1)
-     {
-       current += 2*PAGE_SIZE;
-       current += 16*sizeof(int);
+    if (DSM_NODE_ID == 0)
+      {
+        current += 4*sizeof(int);
+        value = *((int *)current);
+        printf("[%i] valeur de l'entier : %i\n", DSM_NODE_ID, value);
+      }
+    else if (DSM_NODE_ID == 1)
+      {
+        current += 2*PAGE_SIZE;
+        current += 16*sizeof(int);
 
-       value = *((int *)current);
-       printf("[%i] valeur de l'entier : %i\n", DSM_NODE_ID, value);
-     }
+        value = *((int *)current);
+        printf("[%i] valeur de l'entier : %i\n", DSM_NODE_ID, value);
+      }
     dsm_finalize();
     sleep(2);
     printf("----------------------FIN EXEMPLE-------------------------\n");
     fflush(stdout);
-   return 1;
+    return 1;
 }
